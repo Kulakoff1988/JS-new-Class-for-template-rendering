30311a446fa635dcee266d97d68869cb7fe5659d
Kulakoff1988 <44964906+Kulakoff1988@users.noreply.github.com>
2019-02-09 20:24:35 +0300

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,63 +1,43 @@
-/**
- * @typedef {object} ProjectObject
- * @property {string} Name
- * @property {number} Age
- * @property {string} Comment
- */
-
 class Project1 {
     constructor({   Target = void 0,
                     Users = [],
                 }) {
         this.Users = Users;
         this.Target = Target;
-        for (let i = 0; i < Users.length; i++) {
-            let newTr = document.createElement('tr');
-            let newTd = document.createElement('td');
-            newTd.innerText = Users[i].Name;
-            newTr.appendChild(newTd);
-            let newTd1 = document.createElement('td');
-            newTd1.innerText = Users[i].Age;
-            newTr.appendChild(newTd1);
-            let newTd2 = document.createElement('td');
-            newTd2.innerText = Users[i].Comment;
-            newTr.appendChild(newTd2);
-            this.Target.appendChild(newTr);
-        };
+        this.initialRender();
     }
     get Slaves() {
      return this.Users;
     }
     set Slaves(data) {
         this.Users = data;
+        //дописать код обновления таблицы;
     }
-    /**
-     *
-     * @param {ProjectObject} Data
-     */
     Add (item) {
         this.Users.push(item);
-        let newTr = document.createElement('tr');
-        let newTd = document.createElement('td');
-        newTd.innerText = item.Name;
-        newTr.appendChild(newTd);
-        let newTd1 = document.createElement('td');
-        newTd1.innerText = item.Age;
-        newTr.appendChild(newTd1);
-        let newTd2 = document.createElement('td');
-        newTd2.innerText = item.Comment;
-        newTr.appendChild(newTd2);
-        this.Target.appendChild(newTr);
+        this.addLine(item.Name, item.Age, item.Comment);
     };
 
     RemoveAll () {
         this.Users = [];
     }
-
-    /**
-     *
-     * @param {Element} Target
-     * @param {ProjectObject[]} [Data]
-     */
-
+    initialRender () {
+        for (let i = 0; i < this.Users.length; i++) {
+            this.addLine(this.Users[i].Name, this.Users[i].Age, this.Users[i].Comment);
+        }
+    }
+    addLine (col1, col2, col3) {
+        let newTr = document.createElement('tr');
+        let newTd = document.createElement('td');
+        newTd.innerText = col1;
+        newTr.appendChild(newTd);
+        let newTd1 = document.createElement('td');
+        newTd1.innerText = col2;
+        newTr.appendChild(newTd1);
+        let newTd2 = document.createElement('td');
+        newTd2.innerText = col3;
+        newTr.appendChild(newTd2);
+        this.Target.appendChild(newTr);
+    };
 }
+
