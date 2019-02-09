9a3488a55bdaedc7fec1f1144ab41f12a2888b36
Kulakoff1988 <44964906+Kulakoff1988@users.noreply.github.com>
2019-02-09 21:27:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-const elem = document.querySelector('#add');
 const sing = document.querySelector('#table');
-const name = document.querySelector('#name');
-const age = document.querySelector('#age');
-const comment = document.querySelector('#comment');
-
-//let Project1 = requre('SlaveControlle');
 
 const newData = new Project1({
     Target: sing,
     Users: []
 });
 
-elem.addEventListener('click', () => newData.Add({Name: name.value, Age: age.value, Comment: comment.value}));
-document.addEventListener('keydown', evt => {
-    if(evt.keyCode === 13) {
-        return newData.Add({Name: name.value, Age: age.value, Comment: comment.value});
-    }
-});
-
 newData.Add({Name: 'Gustav', Age: 12, Comment: 'best courier'});
-newData.Slaves = [{Name: 'Nigel', Age: 12, Comment: 'best courier'}];
+newData.Slaves = [{Name: 'Niger', Age: 12, Comment: 'best courier'}];
