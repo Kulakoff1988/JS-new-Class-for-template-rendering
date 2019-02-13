e57e806a17a5d11c76509ca548cc6233d312b49e
Alexander Kulakov <kulakofft4@gmail.com>
2019-02-13 15:29:13 +0300

    Bug fix, interface refactoring
    исправлена работа методов RemoveAll, set; добавлена функция отображения значений по умолчанию, если он не были заданы; функции с отсутствующим контекстом вынесены из интерфейса класса

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
+
 const sing = document.querySelector('#table');
+
+
 
 const newData = new Project1 ({
     Target: sing,
     Template: `<div class="user-card">
                   <div class="caption">%Name%</div>
                   <input class="date-of-birth" type="text" value="%Age%">
-                  <textarea class="textareat">%Comment%</textarea>   
-                  <button class="button">Save</button>
+                  <textarea class="textarea">%Comment%</textarea>   
+                  <button class="button p1-btn-save">Save</button>
                 </div>`,
     Users: []
 });
 
 
-newData.Slaves = [{Name: 'Niger', Age: 25, Comment: 'best courier'}, {Name: 'Gustav', Age: 21, Comment: 'best seller'}];
+newData.Slaves = [{Name: 'Niger', Age: 25, Comment: 'best courier'}, {Name: 'Tor', Age: 80, Comment: 'God'}];
--- app1.js
+++ app1.js
@@ -1,3 +1,29 @@
+const   elem = document.querySelector('#add'),                              // кнопка добавления элементов из заполненных форм
+        name = document.querySelector('#name'),                             // форма заполнения имени
+        age = document.querySelector('#age'),                               // форма заполнения возраста
+        comment = document.querySelector('#comment'),                       // форма заполнения комментария
+        htmlOfButton = '<input type="button" value="Remove" id="remove">',  // шаблон кнопки удаления в HTML представлении
+
+        clearForm =() => {                                                  //функция очистки форм
+            name.value = '';
+            age.value = '';
+            comment.value = '';
+        },
+
+        replacer = (item, template) => {                                           // функция создания создания строки с требуемыми словами из шаблона
+            for (let key of Object.getOwnPropertyNames(item)) {                     // принимает на вход объект с набором необходимых значений ключей
+                template = template.replace(new RegExp(`%${key}%`), item[key]);     // возвращает строку
+            }
+            return template;
+        },
+
+        createDOM = (tagValue, tagName = 'div') => {                    // функция создания DOM-элемента, принимает на вход строку с HTML-содержанием
+            const DOM_name = document.createElement(tagName);           // будущего элемента
+            DOM_name.innerHTML = tagValue;                              // второй параметр передавать не обязательно (он затирается во время выполнения функции)
+            return DOM_name.firstChild;                                 // возвращает DOM-элемент
+        };
+
+
 class Project1 {
     constructor({   Target = void 0,
                     Template = ``,
@@ -6,73 +32,66 @@ class Project1 {
         this.Users = Users;
         this.Target = Target;
         this.Template = Template;
-        
-        const   doc = document,
-                elem = doc.querySelector('#add'),
-                name = doc.querySelector('#name'),
-                age = doc.querySelector('#age'),
-                comment = doc.querySelector('#comment'),
-                clearForm = () => {
-                    name.value = '';
-                    age.value = '';
-                    comment.value = '';
-                };
 
-        elem.addEventListener('click', () => {
-            this.Add({Name: name.value, Age: age.value, Comment: comment.value})
+        elem.addEventListener('click', () => {                          // триггер по клику на кнопке "Add", вызов метода Add и функции очистки форм
+            this.Add({  Name: name.value,
+                        Age: age.value,
+                        Comment: comment.value});
             clearForm()
         });
 
-        doc.addEventListener('keydown', evt => {
+        document.addEventListener('keydown', evt => {                   // триггер по нажатию Enter, вызов метода Add и функции очистки форм
             if(evt.keyCode === 13) {
                 this.Add({Name: name.value, Age: age.value, Comment: comment.value});
                 clearForm()
             }
         });
-     
-        this._initialRender();
+
     }
     
-    get Slaves() {
+    get Slaves() {                                                      // получение списка пользователей в виде массива объектов
      return this.Users;
     }
     
-    set Slaves(data) {
+    set Slaves(data) {                                                  // присваивание данных списку пользователей
         this.Users = data;
-        this._initialRender();
+        this._removeCurrentRendering();                                 // вызов очистки текущего рендеринга списка пользователей
+        this._render();                                                 // вызов функции рендеринга
     }
     
-    Add (item) {
-        this.Users.push(item);
-        this.Target.appendChild(this._addLine(item));
+    Add (item) {                                                        // добавление нового пользователя, принимает на вход объект
+        this.Users.push(item);                                          // добавление нового пользователя в общий список пользователей
+        this.Target.appendChild(this._addElementRendering({             // вызов метода отображения нового элемента
+            Name: item.Name || 'Not specified',
+            Age: item.Age || 'Not specified',
+            Comment: item.Comment || 'Not specified'
+        }));
     };
 
-    RemoveAll () {
-        this.Users = [];
+    RemoveAll () {                                                      // очистка списка пользователей, также очищает текущий рендеринг
+        this.Users.length = 0;
+        this._removeCurrentRendering();
     }
-    
-    _initialRender () {
-        for (let u of this.Users) {
-            this.Target.appendChild(this._addLine(u));
+
+    _render () {                                                        // рендеринг страницы на основании полученных данных из списка пользователей
+        for (let u of this.Users) {                                     // и шаблона
+            this.Target.appendChild(this._addElementRendering(u));
         }
     }
+
+    _removeCurrentRendering (parent = this.Target) {                    // очистка текущего рендеринга списка пользователей
+        while (parent.firstChild) parent.removeChild(parent.lastChild);
+    }
     
-    _addLine (user) {
-        let replaceStr = this.Template;
-        for (let i of Object.keys(user)) {
-            replaceStr = replaceStr.replace(new RegExp(`%${i}%`), user[i]);
-        }
-        const replaceNode = document.createElement('div');
-        replaceNode.innerHTML = replaceStr;
-        const replaceChild = replaceNode.firstChild;
-        const remove = document.createElement('input');
-        remove.innerHTML = `<input type="button" value="Remove" id="remove">`;
-        const removeButton = remove.firstChild;
-        replaceChild.insertAdjacentElement('beforeend', removeButton);
+    _addElementRendering (user) {                                       // добавление рендеринга элемента, принимает на вход объект с данными пользователя
+        const replaceStr = this.Template;
+        const newLine = createDOM(replacer(user, replaceStr));          // создаём DOM-элемент из данных пользователя по шаблону
+        const removeButton = createDOM(htmlOfButton);                   // создаём кнопку удаления элемента
+        newLine.insertAdjacentElement('beforeend', removeButton);
         removeButton.addEventListener('click', user => {
             this.Users.splice(this.Users.indexOf(user), 1);
-            this.Target.removeChild(replaceChild);
+            this.Target.removeChild(newLine);
         });
-        return replaceChild;
+        return newLine;
     };
 }
